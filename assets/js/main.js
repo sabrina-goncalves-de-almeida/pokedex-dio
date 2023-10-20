4558f9618f6fbc197398d2e480d80216df94dc57
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-10-20 00:33:00 -0300

    feat(#1): acrecentando cath e arrow function ao callback

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,7 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}$limit=${limit}`;
 
-fetch(url).then(function (response){
-    console.log(response)
-});
-
-
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
