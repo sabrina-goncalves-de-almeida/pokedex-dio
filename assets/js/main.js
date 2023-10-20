6d06ac6640d0994e9f03a03802252209d8a1a3e2
sabrina-goncalves-de-almeida <sabrina.goncalves@dcx.ufpb.br>
2023-10-20 01:13:40 -0300

    feat(#1): pegando os itens da API e transformando em itens de lista para HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,33 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}$limit=${limit}`;
 
+function convertPokemonToLi(pokemon){
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>      
+        </li>
+    `
+}
+
+const pokemonsList = document.getElementById("pokemonsList");
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        debugger
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonsList.innerHTML += convertPokemonToLi(pokemon)
+
+        }
+    })
     .catch((error) => console.error(error))
